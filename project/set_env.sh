768181343efb059c9fa24de6fe8d3fcc5d7cf39d
kikic <kikic@Ahmeds-MacBook-Air-2.local>
2021-07-31 09:28:10 +0100

    docker

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=kikic
+export POSTGRES_PASSWORD=a7med123
+export POSTGRES_HOST=cdr2.cztskm8s0ipi.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-129309386521-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
 export URL=http://localhost:8100
